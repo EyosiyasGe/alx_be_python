c32cc0d5bb307e81a9e5aa2673bb9d0dbfd731fc
EyosiyasGe <eyosiyasgeze@gmail.com>
2025-06-02 22:10:14 +0300

    Forth week

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = int(input("Enter a number to see its multiplication table:"))
 for a in range(1,11) :
     result = number * a
-    print ( number, "* ", a ," =", result )
+    print ( f"{number} * {a}  = {result}" )
     a +=1
