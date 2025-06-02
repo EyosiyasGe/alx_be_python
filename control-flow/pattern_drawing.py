d0455a677d1a54cb958fd0e3140dca4ae3ae9116
EyosiyasGe <eyosiyasgeze@gmail.com>
2025-06-02 22:56:08 +0300

    Forth week

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,7 @@
-number = int(input("Enter the size of the pattern:"))
+numbers = int(input("Enter the size of the pattern:"))
 i = 0
-while i <= number :
-    print ("*" * number)
-    
+while i < numbers :
+    for j in range(numbers) :
+        print ("*" , end="") 
+    i +=1
+    print()
