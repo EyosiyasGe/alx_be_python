91a3ed208c159789734b725fea528bbc91878e7b
EyosiyasGe <eyosiyasgeze@gmail.com>
2025-06-05 21:31:15 +0300

    Forth week

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'division'  :
-        if num1 > 0 and num2 > 0 :
+        if num2 != 0 :
             return num1/num2 
         else :
             return print("0 division is impossible")
