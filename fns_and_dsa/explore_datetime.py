9f54a2bf77cc12b464b47c17f3f0129d4b0cb0ec
EyosiyasGe <eyosiyasgeze@gmail.com>
2025-06-11 15:36:18 +0300

    Forth week

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
 from datetime import datetime,timedelta
 def display_current_datetime() :
     current_date  = datetime.now()
-    print(current_date.strftime("%Y-%m-%d-%H-%M-%S"))
+    print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
 def calculate_future_date():
     
     days_to_add = timedelta(int(input('Enter the number of days: ')))
