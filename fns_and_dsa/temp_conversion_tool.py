50f4a31743c5f8b37490bb160609a1128cde1245
EyosiyasGe <eyosiyasgeze@gmail.com>
2025-06-11 17:51:57 +0300

    Forth week

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,18 +1,17 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-x = float(input("enter the amount "))
-y= input("Is this temperature in Celsius or Fahrenheit? (C/F) ").lower()
 def convert_to_celsius(fahrenheit) :
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
-    fahrenheit = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
-    return fahrenheit
+     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-    celsius = celsius  * CELSIUS_TO_FAHRENHEIT_FACTOR
-    return celsius
-if y == "c":
-    print(convert_to_fahrenheit(x))
-elif y == "f" :
-    print(convert_to_celsius(x))
-else :
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+try :
+    x = float(input("enter the amount "))
+    y= input("Is this temperature in Celsius or Fahrenheit? (C/F) ").lower()
+    if y == "c":
+        print(convert_to_fahrenheit(x))
+    elif y == "f" :
+        print(convert_to_celsius(x))
+    else :
+        print("Invalid temperature. Please enter a numeric value.")
+except ValueError:
     print("Invalid temperature. Please enter a numeric value.")
